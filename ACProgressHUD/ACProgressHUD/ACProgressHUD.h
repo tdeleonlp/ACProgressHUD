0d0fad2122f4b260053cbf455d70793e389fbda3
Thomas De Leon <tdeleon@liveperson.com>
2018-09-27 09:28:37 -0700

    Creating framework

---
+++ ACProgressHUD/ACProgressHUD/ACProgressHUD.h
@@ -0,0 +1,19 @@
+//
+//  ACProgressHUD.h
+//  ACProgressHUD
+//
+//  Created by Thomas De Leon on 9/13/18.
+//  Copyright © 2018 Abhishek. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ACProgressHUD.
+FOUNDATION_EXPORT double ACProgressHUDVersionNumber;
+
+//! Project version string for ACProgressHUD.
+FOUNDATION_EXPORT const unsigned char ACProgressHUDVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ACProgressHUD/PublicHeader.h>
+
+
